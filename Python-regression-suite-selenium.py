60b880e1e56e723d9b6b95886f691c56f34293b2
mikgle <mikael.glemne@havochvatten.se>
2017-02-10 13:08:40 +0100

    Test case 22 updated with some id tags.

--- Python-regression-suite-selenium.py
+++ Python-regression-suite-selenium.py
@@ -1186,10 +1186,10 @@ class UnionVMSTestCase(unittest.TestCase):
         self.driver.find_element_by_id("uvms-header-menu-item-assets").click()
         time.sleep(5)
         # Click on "saved groups" drop box
-        self.driver.find_element_by_xpath("(//button[@type='button'])[13]").click()
+        self.driver.find_element_by_id("asset-dropdown-saved-search").click()
         time.sleep(2)
         # Click on delete button for Grupp 2
-        self.driver.find_element_by_xpath("//div[@id='content']/div/div[3]/div[2]/div/div/div[3]/div/div/form/div/div/div/div/div/div/div/div[2]/div/div/div/div/ul/li[3]/span").click()
+        self.driver.find_element_by_id("asset-dropdown-saved-search-delete-item-1").click()
         time.sleep(2)
         # Click on confirmation button
         self.driver.find_element_by_css_selector("div.modal-footer > button.btn.btn-primary").click()
@@ -1198,7 +1198,7 @@ class UnionVMSTestCase(unittest.TestCase):
         self.driver.refresh()
         time.sleep(10)
         # Check that Grupp 1 exists and Grupp 2 does not exist
-        self.driver.find_element_by_xpath("(//button[@type='button'])[13]").click()
+        self.driver.find_element_by_id("asset-dropdown-saved-search").click()
         time.sleep(1)
         self.assertEqual(groupName[0], self.driver.find_element_by_link_text(groupName[0]).text)
         try:
