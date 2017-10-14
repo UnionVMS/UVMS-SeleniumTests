07afdfd0ec34d426a39739ec243148cea5685665
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-10-14 21:55:34 +0200

    use full path /var/lib/jenkins/workspace/UVMS-Docker-dev-test-frontend/release-test/target/failsafe-reports

--- DockerUnionVMSparameters.py
+++ DockerUnionVMSparameters.py
@@ -79,4 +79,4 @@ rulesHeadlineNames = ["Rule name", "Last triggered", "Date updated", "Updated by
 
 #Jenkins job settings
 uvmsCheckoutPath = "/tmp/git-uvms-database"
-testResultPath = "failsafe-reports"
+testResultPath = "/var/lib/jenkins/workspace/UVMS-Docker-dev-test-frontend/release-test/target/failsafe-reports"
