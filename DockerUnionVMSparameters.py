047cee7bd4c6edc7941a6827220adbf8eedb1c63
James Sörling <james.p.sorling@consid.se>
2017-10-18 13:59:52 +0200

    fix downloadPath

--- DockerUnionVMSparameters.py
+++ DockerUnionVMSparameters.py
@@ -79,5 +79,5 @@ rulesHeadlineNames = ["Rule name", "Last triggered", "Date updated", "Updated by
 
 #Jenkins job settings
 uvmsCheckoutPath = "/tmp/git-uvms-database"
-downloadPath = "/tmp/"
+downloadPath = "/tmp"
 testResultPath = "/var/lib/jenkins/workspace/UVMS-Docker-dev-test-frontend/release-test/target/failsafe-reports"
