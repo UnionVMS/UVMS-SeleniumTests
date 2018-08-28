655c273d0a917aa414fe7a70aad8ec267736bd02
mikgle <mikael.glemne@havochvatten.se>
2018-08-28 17:26:01 +0200

    test_0303_create_several_additional_channels_for_mobile_terminals added together with data file "channelstomobileterminals3xxxx.csv"

--- Python-regression-suite-selenium.py
+++ Python-regression-suite-selenium.py
@@ -358,6 +358,7 @@ def create_one_new_asset_from_gui_with_parameters(self, parameterList):
 
 
 def create_one_new_mobile_terminal_from_gui(self, mobileTerminalNumber):
+    # Click on mobile terminal tab
     self.driver.find_element_by_id("uvms-header-menu-item-communication").click()
     time.sleep(2)
     # Click on new terminal button
@@ -520,6 +521,91 @@ def create_one_new_mobile_terminal_via_asset_tab_with_parameters(self, vesselNam
     # Leave new asset view
     self.driver.find_element_by_id("menu-bar-cancel").click()
     time.sleep(2)
+
+
+
+def create_one_new_channel_for_one_mobile_terminal(self, channelRow, referenceDateTimeValue):
+    # Click on mobile terminal tab
+    self.driver.find_element_by_id("uvms-header-menu-item-communication").click()
+    time.sleep(2)
+    # Search for mobile terminal via serial number
+    self.driver.find_element_by_id("mt-input-search-serialNumber").clear()
+    self.driver.find_element_by_id("mt-input-search-serialNumber").send_keys(channelRow[0])
+    self.driver.find_element_by_id("mt-btn-advanced-search").click()
+    time.sleep(2)
+    # Click on detail button
+    self.driver.find_element_by_id("mt-toggle-form").click()
+    time.sleep(1)
+    # Click on link "Add new channel"
+    self.driver.find_element_by_id("mt-0-addChannel").click()
+    time.sleep(1)
+    # Enter channel name
+    self.driver.find_element_by_id("mt-0-channel-1-communicationChannel").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-communicationChannel").send_keys(channelRow[1])
+    # Activate Poll if value is "true"
+    if channelRow[2] == "1":
+        self.driver.find_element_by_id("mt-0-channel-1-checkbox-polling").click()
+        self.driver.find_element_by_xpath("//div[@id='content']/div/div[3]/div[2]/div/div/div/div/div/div[4]/div/div[2]/form/fieldset/div/div[3]/div[4]/div/div[2]/div[2]/label").click()
+    # Activate Config if value is "true"
+    if channelRow[3] == "1":
+        self.driver.find_element_by_id("mt-0-channel-1-checkbox-config").click()
+        self.driver.find_element_by_xpath("//div[@id='content']/div/div[3]/div[2]/div/div/div/div/div/div[4]/div/div[2]/form/fieldset/div/div[3]/div[4]/div/div[2]/div[3]/label").click()
+    # Activate Default if value is "true"
+    if channelRow[4] == "1":
+        self.driver.find_element_by_id("mt-0-channel-1-checkbox-default").click()
+        self.driver.find_element_by_xpath("//div[@id='content']/div/div[3]/div[2]/div/div/div/div/div/div[4]/div/div[2]/form/fieldset/div/div[3]/div[4]/div/div[2]/div[4]/label").click()
+    # Enter DNID value
+    self.driver.find_element_by_id("mt-0-channel-1-dnid").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-dnid").send_keys(channelRow[5])
+    # Enter Member Number
+    self.driver.find_element_by_id("mt-0-channel-1-memberId").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-memberId").send_keys(channelRow[6])
+    # Enter Land station
+    self.driver.find_element_by_id("mt-0-channel-1-lesDescription").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-lesDescription").send_keys(channelRow[7])
+    # Enter Start Date/Time based on deltaHourValue from file
+    tempTimeValue = referenceDateTimeValue + datetime.timedelta(hours=int(channelRow[8]))
+    self.driver.find_element_by_id("mt-0-channel-1-started").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-started").send_keys(tempTimeValue.strftime("%Y-%m-%d %H:%M:%S"))
+    # Enter Stop Date/Time based on deltaHourValue from file
+    tempTimeValue = referenceDateTimeValue + datetime.timedelta(hours=int(channelRow[9]))
+    self.driver.find_element_by_id("mt-0-channel-1-stopped").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-stopped").send_keys(tempTimeValue.strftime("%Y-%m-%d %H:%M:%S"))
+    # Enter Installer from file
+    self.driver.find_element_by_id("mt-0-channel-1-installedBy").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-installedBy").send_keys(channelRow[10])
+    # Enter Installed Date/Time based on deltaHourValue from file
+    tempTimeValue = referenceDateTimeValue + datetime.timedelta(hours=int(channelRow[11]))
+    self.driver.find_element_by_id("mt-0-channel-1-installedOn").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-installedOn").send_keys(tempTimeValue.strftime("%Y-%m-%d %H:%M:%S"))
+    # Enter Uninstalled Date/Time based on deltaHourValue from file
+    tempTimeValue = referenceDateTimeValue + datetime.timedelta(hours=int(channelRow[12]))
+    self.driver.find_element_by_id("mt-0-channel-1-uninstalled").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-uninstalled").send_keys(tempTimeValue.strftime("%Y-%m-%d %H:%M:%S"))
+    # Enter Exp. frequency from file
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyExpected").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyExpected").send_keys(channelRow[13])
+    # Enter Grace period from file
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyGrace").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyGrace").send_keys(channelRow[14])
+    # Enter In port from file
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyPort").clear()
+    self.driver.find_element_by_id("mt-0-channel-1-frequencyPort").send_keys(channelRow[15])
+
+    # Click on Save button
+    self.driver.find_element_by_id("menu-bar-update").click()
+    time.sleep(2)
+    # Enter Comment in comment field
+    self.driver.find_element_by_name("comment").clear()
+    self.driver.find_element_by_name("comment").send_keys(commentValue)
+    time.sleep(1)
+    # Click on Update button
+    self.driver.find_element_by_css_selector("div.modal-footer > div.row > div.col-md-12 > button.btn.btn-primary").click()
+    time.sleep(3)
+    # Click on Cancel
+    self.driver.find_element_by_id("menu-bar-cancel").click()
+    time.sleep(3)
+
 
 
 
@@ -1424,6 +1510,24 @@ def create_mobileterminal_from_file_based_on_link_file(self, assetFileName, mobi
         assetVesselName = get_selected_asset_column_value_based_on_cfr(assetAllrows, linkAssetMobileTerminalAllrows[x][1], 1)
         mobileTerminalRawValue = get_selected_Mobile_terminal_raw_based_on_serialNumber(mobileTerminalAllrows, linkAssetMobileTerminalAllrows[x][0])
         create_one_new_mobile_terminal_via_asset_tab_with_parameters(self, assetVesselName, mobileTerminalRawValue)
+
+
+
+def create_addtional_channels_for_mobileterminals_from_file(self, channelFileName):
+    # Create addtional channels for Mobile Terminals from file based on channelFile
+
+    # Set referenceDateTime to current UTC time
+    referenceDateTime = datetime.datetime.utcnow()
+
+    # Open saved csv file and read all asset elements
+    channelAllrows = get_elements_from_file(channelFileName)
+
+
+    # create_one new channel for mentioned mobile terminal
+    for x in range(0, len(channelAllrows)):
+        create_one_new_channel_for_one_mobile_terminal(self, channelAllrows[x], referenceDateTime)
+
+
 
 
 
@@ -4811,6 +4915,14 @@ class UnionVMSTestCaseMobileTerminalChannels(unittest.TestCase):
             tempStatusValue = statusValue[int(mobileTerminalAllrows[x][14])]
             tempStatusValue = tempStatusValue.upper()
             self.assertEqual(tempStatusValue, self.driver.find_element_by_xpath("//div[@id='content']/div/div[3]/div[2]/div/div/div/div/div[3]/div/div/div/div/span/table/tbody/tr[" + str(x+1) + "]/td[9]/span").text)
+
+
+
+    @timeout_decorator.timeout(seconds=180)
+    def test_0303_create_several_additional_channels_for_mobile_terminals(self):
+        # Create assets from file with several different values for filtering
+        create_addtional_channels_for_mobileterminals_from_file(self, 'channelstomobileterminals3xxxx.csv')
+
 
 
 if __name__ == '__main__':
